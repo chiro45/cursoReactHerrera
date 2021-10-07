282b350f6e25071988c3b12cc4dd554c0d2c6d67
chiro45 <luciano.chiroli45@gmail.com>
2021-10-07 19:01:17 -0300

    segundo commit

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -13,6 +13,7 @@ const PrimeraApp = ({saludo, subtitulo }) =>{
             <h1> { saludo } </h1>
                 {/* <pre>{JSON.stringify(saludo ,null, 3)}</pre>  */}
             <p> {subtitulo}</p>
+            <h3>Hello</h3>
         </>
     );
 }
